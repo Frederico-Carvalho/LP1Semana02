371504a1fe7e3a1cd983375fafede93d0a36d7b9
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-28 18:00:11 +0000

    Break Done

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,7 +6,23 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string question, response;
+
+            Console.Write("Place your question? or Write EXIT to leave: ");
+            question = Console.ReadLine();
+
+            switch (question)
+            {
+                case "BREAK":
+                    response = "Bye!";
+                    break;
+                
+                default:
+                    response = "You got me, I'm not THAT smart!";
+                    break;
+            }
+
+            Console.WriteLine(response);
         }
     }
 }

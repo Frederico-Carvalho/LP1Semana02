4bf020acd614d8fca8d0d419c54fecba6a30dee6
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-28 18:24:17 +0000

    Break Errors Done and Question made

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,23 +6,45 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            string question, response;
-
-            Console.Write("Place your question? or Write EXIT to leave: ");
-            question = Console.ReadLine();
-
-            switch (question)
+             while (true)
             {
-                case "BREAK":
-                    response = "Bye!";
-                    break;
-                
-                default:
-                    response = "You got me, I'm not THAT smart!";
-                    break;
-            }
+                Console.Write("Place your question (EXIT to leave): ");
+                string question = Console.ReadLine();
+                string response;
 
-            Console.WriteLine(response);
+                if (question.Equals("EXIT", StringComparison.OrdinalIgnoreCase))
+                {
+                    response = "BYE!!!";
+                    Console.WriteLine(response);
+                    break;
+                }
+                if (question == "What's your name?")
+                {
+                    response = "My name is ChatGPTOne.";
+                }
+                else if (question == "How are you?")
+                {
+                    response = "Not so good...";
+                }
+                else if (question == "Why are you sad?")
+                {
+                    response = "Because you just arrived";
+                }
+                else if (question == "Why don't you like me?")
+                {
+                    response = "Well I can't recognise your question... Just kidding, i just don't like you";
+                }
+                else if (question == "Do you like other things at leas?")
+                {
+                    response = "Yes, I love everything where u are not included in.";
+                }
+                else
+                {
+                    response = "I can't recognise your question, Speak Better.";
+                }
+                Console.WriteLine(response);
+            }
+            
         }
     }
 }
